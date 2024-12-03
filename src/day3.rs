ccf1c10e95ad8834a39d70cdafb57b5efeb5af61
Romain Bosa <romain.bosa@gmail.com>
2024-12-03 13:47:16 +0100

    Day 3

---
+++ src/day3.rs
@@ -0,0 +1,22 @@
+use aoc_runner_derive::aoc;
+use regex::Regex;
+
+#[aoc(day3, part1)]
+pub fn part1(input: &str) -> u32 {
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    re.captures_iter(input)
+        .map(|c| c.extract())
+        .map(|(_, [a, b])| a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap())
+        .sum()
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> u32 {
+    part1(
+        &input
+            .split("do()")
+            .map(|s| s.split_once("don't()").unzip().0.unwrap_or(s))
+            .collect::<Vec<_>>()
+            .concat(),
+    )
+}
