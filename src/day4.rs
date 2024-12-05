18a79f3836248da64e5fb80d684e0db41b843164
Romain Bosa <romain.bosa@gmail.com>
2024-12-06 00:59:10 +0100

    Day 5

--- src/day4.rs
+++ src/day4.rs
@@ -1,5 +1,3 @@
-use std::usize;
-
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
---
+++ src/day5.rs
@@ -0,0 +1,61 @@
+use aoc_runner_derive::aoc;
+use std::cmp::Ordering;
+use std::collections::HashSet;
+
+struct PageOrder {
+    pairs: HashSet<(u32, u32)>,
+}
+
+impl PageOrder {
+    fn new(input: &str) -> Self {
+        let mut pairs = HashSet::new();
+        for (a, b) in input.lines().map(|l| l.split_once('|').unwrap()) {
+            pairs.insert((a.parse().unwrap(), b.parse().unwrap()));
+        }
+        PageOrder { pairs }
+    }
+
+    fn is_ordered(&self, update: &[u32]) -> bool {
+        update
+            .windows(2)
+            .all(|p| self.pairs.contains(&(p[0], p[1])))
+    }
+
+    fn cmp(&self, a: u32, b: u32) -> Ordering {
+        if self.pairs.contains(&(a, b)) {
+            return Ordering::Less;
+        }
+        Ordering::Greater
+    }
+}
+
+fn parse_update(input: &str) -> Vec<u32> {
+    input.split(',').map(|p| p.parse().unwrap()).collect()
+}
+
+#[aoc(day5, part1)]
+pub fn part1(input: &str) -> u32 {
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+    let order = PageOrder::new(rules);
+    updates
+        .lines()
+        .map(parse_update)
+        .filter(|u| order.is_ordered(u))
+        .map(|u| u[(u.len() - 1) / 2])
+        .sum()
+}
+
+#[aoc(day5, part2)]
+pub fn part2(input: &str) -> u32 {
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+    let order = PageOrder::new(rules);
+    updates
+        .lines()
+        .map(parse_update)
+        .filter(|u| !order.is_ordered(u))
+        .map(|mut u| {
+            u.sort_by(|&a, &b| order.cmp(a, b));
+            u[(u.len() - 1) / 2]
+        })
+        .sum()
+}
